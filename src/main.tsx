0599f7d24fa20d01b6c9a9804f3cd298993e84f0
Eric Larson <ewlarson@gmail.com>
2025-01-25 11:07:19 -0600

    Bug fix: styles for leaflet opacity and fullscreen controls

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ApiProvider } from './context/ApiContext';
 import App from './App.tsx';
 import './index.css';
+import './styles/leaflet.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
