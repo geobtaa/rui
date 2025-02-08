feae7b6c9b3376166feb7f970172316af27e5015
Eric Larson <ewlarson@gmail.com>
2025-02-08 10:25:24 -0600

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -62,7 +62,7 @@ export function SearchPage() {
       <div className="min-h-screen flex flex-col">
         <Header />
 
-        <main className="flex-1 bg-gray-50">
+        <main className="flex-1 bg-gray-50 pb-8 mb-8">
           <div className="w-full px-4 sm:px-6 lg:px-8 pt-6">
             <SearchConstraints 
               facets={facets}
